055f7f8f0480045e68442c68d2793fbbead75a83
Robin Richardson <raarichardson@gmail.com>
2020-03-09 10:19:03 +0100

    Add more RDF triples

--- playground/fairworkflow.py
+++ playground/fairworkflow.py
@@ -1,3 +1,13 @@
+class RDFtriple:
+    def __init__(self, sub, pred, obj):
+        self.sub = sub
+        self.pred = pred
+        self.obj = obj
+
+    def __str__(self):
+        return '  '.join([self.sub, self.pred, self.obj])
+
+
 class FairWorkflow:
     def __init__(self, name='newworkflow'):
         self.name = 'plex:' + name
@@ -17,16 +27,23 @@ class FairWorkflow:
         return self.flow[-1].get_result()
 
     def __str__(self):
-        return "\n".join([str(s) for s in self.flow])
+        """
+            Build RDF metadata for entire FAIR Workflow
+        """
+        meta = ''
+        if len(self.flow) > 0:
 
-class RDFtriple:
-    def __init__(self, sub, pred, obj):
-        self.sub = sub
-        self.pred = pred
-        self.obj = obj
+            # Workflow metadata
+            plan_rdf = []
+            plan_rdf.append(RDFtriple(self.name, 'rdf:type', 'dul:workflow'))
+            plan_rdf.append(RDFtriple(self.name, 'pwo:hasFirstStep', self.flow[0].name))
+            meta += '\n'.join([str(s) for s in plan_rdf]) + '\n\n'
 
-    def __str__(self):
-        return '  '.join([self.sub, self.pred, self.obj])
+            # Steps metadata
+            meta += "\n".join([str(s) for s in self.flow]) + '\n'
+
+        return meta
+
 
 class FairStepEntry:
     def __init__(self, name, func, args, kwargs, metadata):
@@ -66,32 +83,36 @@ class FairStepEntry:
         metastr = '\n'.join([str(s) for s in self.metadata])
         return f"{metastr}\n"
 
+
 def FairStep(fairworkflow):
-    def fairdecorator(func):
-        def wrappedfn(*args, **kwargs):
+    def fair_wrapper(func):
+        def metadata_wrapper(*args, **kwargs):
 
             # Autogenerate metadata
             metadata = []
 
             stepname = "plex:" + func.__name__
 
+            metadata.append(RDFtriple(stepname, "edam:type", "p-plan:Step"))
             metadata.append(RDFtriple(stepname, "rdf:type", "p-plan:Step"))
             metadata.append(RDFtriple(stepname, "rdf:type", "bpmn:scriptTask"))
             metadata.append(RDFtriple(stepname, "p-plan:isStepOfPlan", fairworkflow.name))
             
             for var, arg in zip(func.__code__.co_varnames, args):
+                metadata.append(RDFtriple('plex:' + var, 'rdf:type', 'p-plan:Variable'))
                 metadata.append(RDFtriple(stepname, "p-plan:hasInputVar", 'plex:' + var))
 
                 if isinstance(arg, FairStepEntry):
                     metadata.append(RDFtriple('plex:' + var, "p-plan:isOutputVarOf", arg.name))
+                    metadata.append(RDFtriple(arg.name, "dul:precedes", stepname))
 
             if func.__doc__:
-                metadata.append(RDFtriple("description", func.__doc__.strip()))
+                metadata.append(RDFtriple(stepname, "dc:description", f'"{func.__doc__.strip()}"'))
 
             fairstep = FairStepEntry(stepname, func, args, kwargs, metadata)
 
             fairworkflow.add_step(fairstep)
 
             return fairstep
-        return wrappedfn
-    return fairdecorator
+        return metadata_wrapper
+    return fair_wrapper
--- playground/test_fw.py
+++ playground/test_fw.py
@@ -1,18 +1,29 @@
 from fairworkflow import FairWorkflow, FairStep
 
-fw = FairWorkflow(name='basicworkflow')
+fw = FairWorkflow(name='add_and_multiply_workflow')
 
 @FairStep(fw)
 def add(int1, int2):
+    """
+        Add two integers together (int1 and int2).
+    """
     result = int1 + int2
     return result
 
 @FairStep(fw)
 def mult(int1, int2):
+    """
+        Multiply two integers together (int1 and int2).
+    """
     result = int1 * int2
     return result
 
 
+
+
+
+
+# Execute workflow
 
 input1 = 3
 input2 = 5
@@ -24,7 +35,7 @@ output2 = mult(output1, input2)
 print("metadata:\n")
 print(fw)
 
-print("Executing workflow")
+print("Executing workflow:")
 result = fw.execute()
 
 print("Result:", result)
