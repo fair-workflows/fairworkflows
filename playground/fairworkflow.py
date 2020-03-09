f4e10c2e28b733a38fd5dd9d6deb10a69e1721a1
Robin Richardson <raarichardson@gmail.com>
2020-03-09 17:41:26 +0100

    Refactor all RDF triple code to use rdflib

--- playground/fairworkflow.py
+++ playground/fairworkflow.py
@@ -1,16 +1,18 @@
-class RDFtriple:
-    def __init__(self, sub, pred, obj):
-        self.sub = sub
-        self.pred = pred
-        self.obj = obj
+import rdflib
+from rdflib.namespace import RDF, DC
 
-    def __str__(self):
-        return '  '.join([self.sub, self.pred, self.obj])
+PPLAN = rdflib.Namespace("http://purl.org/net/p-plan/")
+PLEX = rdflib.Namespace("https://plex.org/")
+EDAM = rdflib.Namespace("http://edamontology.org/")
+PROV = rdflib.Namespace("http://www.w3.org/ns/prov/")
+DUL = rdflib.Namespace("http://ontologydesignpatterns.org/wiki/Ontology:DOLCE+DnS_Ultralite/")
+BPMN = rdflib.Namespace("https://www.omg.org/spec/BPMN/")
+PWO = rdflib.Namespace("http://purl.org/spar/pwo/")
 
 
 class FairWorkflow:
     def __init__(self, name='newworkflow'):
-        self.name = 'plex:' + name
+        self.this_workflow = PLEX[name]
         self.steps = []
 
     def add_step(self, fairstep):
@@ -26,51 +28,53 @@ class FairWorkflow:
 
         return self.steps[-1].get_result()
 
-    def __str__(self):
+    def get_rdf(self):
         """
             Build RDF metadata for entire FAIR Workflow
         """
-        meta = ''
-        if len(self.steps) > 0:
 
-            # Prefix header
-            meta += "@prefix rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>\n"
-            meta += "@prefix p-plan: <http://purl.org/net/p-plan#>\n"
-            meta += "@prefix plex <https://github.com/perma-id/w3id.org/tree/master/fair/plex>\n"
-            meta += "@prefix edam <http://edamontology.org/>\n"
-            meta += "@prefix prov: <http://www.w3.org/ns/prov#>\n"
-            meta += "@prefix dul: <http://ontologydesignpatterns.org/wiki/Ontology:DOLCE+DnS_Ultralite>\n"
-            meta += "@prefix bpmn <https://www.omg.org/spec/BPMN/>\n"
-            meta += "\n"
+        # Create an rdf graph to add workflow metadata to.
+        rdf = rdflib.Graph()
+
+        # Bind all non-standard prefixes in use
+        rdf.bind("p-plan", PPLAN)
+        rdf.bind("plex", PLEX)
+        rdf.bind("edam", EDAM)
+        rdf.bind("prov", PROV)
+        rdf.bind("dul", DUL)
+        rdf.bind("bpmn", BPMN)
+
+        # Add steps metadata
+        if len(self.steps) > 0:
 
             # Workflow metadata
             first_step = self.steps[0]
-            plan_rdf = []
-            plan_rdf.append(RDFtriple(self.name, 'rdf:type', 'dul:workflow'))
-            plan_rdf.append(RDFtriple(self.name, 'pwo:hasFirstStep', first_step.name))
+            rdf.add( (self.this_workflow, RDF.type, DUL.workflow) )
+            rdf.add( (self.this_workflow, PWO.hasFirstStep, first_step.this_step) )
             for var, arg in zip(first_step.func.__code__.co_varnames, first_step.args):
-                varname = "plex:" + var
-                binding = varname + "#" + str(arg)
-                plan_rdf.append(RDFtriple(self.name, "prov:qualifiedUsage", binding))
-                plan_rdf.append(RDFtriple(binding, "rdf:type", "prov:Usage"))
-                plan_rdf.append(RDFtriple(binding, "prov:entity", varname))
-                plan_rdf.append(RDFtriple(binding, "rdf:value", f'"{str(arg)}"^^xsd:{str(type(arg))}'))
+                binding = PLEX[var + '#' + str(arg)]
+                rdf.add((PLEX[var], PROV.qualifiedUsage, binding))
+                rdf.add((binding, RDF.type, PROV.Usage))
+                rdf.add((binding, PROV.entity, PLEX[var]))
+                rdf.add((binding, RDF.value, rdflib.Literal(f'{str(arg)}')))
 
-            meta += '\n'.join([str(s) for s in plan_rdf]) + '\n\n'
+            # Add metadata from all the steps to this rdf graph
+            for step in self.steps:
+                rdf += step.get_rdf()
 
-            # Steps metadata
-            meta += "\n".join([str(s) for s in self.steps]) + '\n'
+        return rdf
 
-        return meta
+    def __str__(self):
+        return self.get_rdf().serialize(format='turtle').decode("utf-8")
 
 
 class FairStepEntry:
-    def __init__(self, name, func, args, kwargs, metadata):
-        self.name = name
+    def __init__(self, rdf_node, func, args, kwargs, rdf_graph):
+        self.this_step = rdf_node
         self.func = func
         self.args = args
         self.kwargs = kwargs
-        self.metadata = metadata
+        self.rdf = rdf_graph
         self.executed = False
         self.result = None
 
@@ -86,7 +90,7 @@ class FairStepEntry:
                 resolved_args.append(a)
                 
         self.result = self.func(*resolved_args, **self.kwargs)
-        print("executing " + self.name)
+        print("executing " + self.func.__name__)
 
         self.executed = True
 
@@ -98,37 +102,39 @@ class FairStepEntry:
     def get_result(self):
         return self.result
 
+    def get_rdf(self):
+        return self.rdf
+
     def __str__(self):
-        metastr = '\n'.join([str(s) for s in self.metadata])
-        return f"{metastr}\n"
+        return self.get_rdf().serialize(format='turtle').decode("utf-8")
 
 
 def FairStep(fairworkflow):
     def fair_wrapper(func):
         def metadata_wrapper(*args, **kwargs):
 
-            # Autogenerate metadata
-            metadata = []
+            # Autogenerate rdf metadata for this step
+            rdf = rdflib.Graph()
 
-            stepname = "plex:" + func.__name__
+            this_step = PLEX[func.__name__]
 
-            metadata.append(RDFtriple(stepname, "rdf:type", "p-plan:Step"))
-            metadata.append(RDFtriple(stepname, "rdf:type", "bpmn:scriptTask"))
-            metadata.append(RDFtriple(stepname, "p-plan:isStepOfPlan", fairworkflow.name))
+            rdf.add((this_step, RDF.type, PPLAN.Step))
+            rdf.add((this_step, RDF.type, BPMN.scriptTask))
+            rdf.add((this_step, PPLAN.isStepOfPlan, fairworkflow.this_workflow))
             
             for var, arg in zip(func.__code__.co_varnames, args):
-                metadata.append(RDFtriple('plex:' + var, 'rdf:type', 'p-plan:Variable'))
-                metadata.append(RDFtriple(stepname, "p-plan:hasInputVar", 'plex:' + var))
+                rdf.add((PLEX[var], RDF.type, PPLAN.Variable))
+                rdf.add((this_step, PPLAN.hasInputVar, PLEX[var]))
 
                 if isinstance(arg, FairStepEntry):
-                    metadata.append(RDFtriple('plex:' + var, "p-plan:isOutputVarOf", arg.name))
-                    metadata.append(RDFtriple(arg.name, "dul:precedes", stepname))
+                    rdf.add((PLEX[var], PPLAN.isOutputVarOf, arg.this_step))
+                    rdf.add((arg.this_step, DUL.precedes, this_step))
 
             if func.__doc__:
-                metadata.append(RDFtriple(stepname, "dc:description", f'"{func.__doc__.strip()}"'))
+                rdf.add((this_step, DC.description, rdflib.Literal(f'"{func.__doc__.strip()}"')))
 
-            fairstep = FairStepEntry(stepname, func, args, kwargs, metadata)
-
+            # Add the new step to the workflow
+            fairstep = FairStepEntry(this_step, func, args, kwargs, rdf)
             fairworkflow.add_step(fairstep)
 
             return fairstep
