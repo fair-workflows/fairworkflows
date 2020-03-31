c536c4275262c8a4aff0a6707375a1bc33c7acb7
Robin Richardson <raarichardson@gmail.com>
2020-03-31 17:48:35 +0200

    Add publish cell button, and change icons

--- jupyter-extension/FAIRWorkflowsExtension/main.js
+++ jupyter-extension/FAIRWorkflowsExtension/main.js
@@ -3,21 +3,37 @@ define([
     'base/js/events'
 ], function(Jupyter, events) {
 
-      // Button to add default cell
-    var add_publish_FAIR_workflow_button = function () {
+    // Button to add default cell
+    var add_FAIR_buttons = function () {
+
+        // Add publish cell button
+        Jupyter.toolbar.add_buttons_group([
+            Jupyter.keyboard_manager.actions.register ({
+                'help': 'Publish cell as FAIR step',
+                'icon': 'fa-truck',
+                'handler': publish_FAIR_step
+            }, 'publish-FAIR-workflow', 'Publish Step')
+        ])
+
+        // Add publish workflow button
         Jupyter.toolbar.add_buttons_group([
             Jupyter.keyboard_manager.actions.register ({
                 'help': 'Publish as FAIR Workflow',
-                'icon': 'fa-comment-o',
+                'icon': 'fa-ship',
                 'handler': publish_FAIR_workflow
             }, 'publish-FAIR-workflow', 'Publish Workflow')
-      ])
+        ])
     }
 
     // Run on start
     function load_ipython_extension() {
-        add_publish_FAIR_workflow_button();
+        add_FAIR_buttons();
     }
+
+    // Publish the selected cell
+    var publish_FAIR_step = function() {
+        alert('Step published!');
+    };
 
     // Publish the whole workflow
     var publish_FAIR_workflow = function() {
