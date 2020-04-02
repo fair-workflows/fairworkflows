8f2fae72387c87b477b6363834fd744b8b60af3d
Djura Smits <djura.smits@gmail.com>
2020-04-02 13:40:34 +0200

    Separated prompting from actually generating workflow

--- cli/cli.py
+++ cli/cli.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 from pathlib import Path
-from typing import List, Dict
+from typing import List, Dict, Union
 from core import rdf, pythongen
 import click
 from jinja2 import Environment, FileSystemLoader, select_autoescape
@@ -16,20 +16,32 @@ def cli():
 @click.option('--target', prompt='Target directory')
 def create_workflow(name, target):
     """
-    Create a new workflow interactively.
-    @param name:
-    @param target:
+        Create a new workflow interactively.
+    :param name:
+    :param target:
+    :return:
     """
     print('Let\'s define the steps!')
     steps = prompt_continuous(['name', 'description', 'input', 'output'])
 
-    # TODO: Add option to specify target dir
+    process_workflow(name, steps, target)
+
+
+def process_workflow(name: str, steps: List[Dict[str, Union[str, List[str]]]], target: Union[str, Path]) -> None:
+    """
+        Create a workflow stub including RDF definition and python stubs.
+    :param name: Name of the workflpw
+    :param steps: The individiual steps. These steps are defined as a list of Dicts that have the following keys:
+        - name: Name of the step
+        - description: Description of the step
+        - inputs: List of str of the input parameterws
+        - output: List of str of the output parameters
+    :param target: Target directory
+    :return:
+    """
     workflow_dir = Path(target) / name
-
     workflow_dir.mkdir()
-
     pythongen.render_python_workflow(steps, workflow_dir)
-
     plex_file = workflow_dir / f'{name}.plex'
     plex_workflow = _create_plex_workflow(name, steps)
     plex_workflow.render(str(plex_file))
@@ -46,8 +58,8 @@ def _create_plex_workflow(name, steps):
 def prompt_continuous(questions: List[str]) -> List[Dict[str, str]]:
     """
     Contiuously prompts for a list of questions until the user doesn't specify an answer anymore.
-    @param questions:
-    @return:
+    :param questions:
+    :return:
     """
     answers = []
     while True:
