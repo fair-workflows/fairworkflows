b7ab1a135cf8df06eec251d7e49ee19cc59bddb7
Robin Richardson <raarichardson@gmail.com>
2020-04-24 16:55:38 +0200

    Add sparql query library

--- fairworkflows/fairworkflows/search.py
+++ fairworkflows/fairworkflows/search.py
@@ -1,10 +1,14 @@
 import ipywidgets as widgets
 from traitlets import Unicode, validate
 from IPython.display import display
+from SPARQLWrapper import SPARQLWrapper
+
+
 
 class Search(widgets.DOMWidget):
 
-    def __init__(self):
+    def __init__(self, server='http://server.nanopubs.lod.labs.vu.nl/'):
+
         searchtext = widgets.Text(
             value='',
             placeholder='Type something',
@@ -15,7 +19,7 @@ class Search(widgets.DOMWidget):
         resultsbox = widgets.Select(
             options=[],
             value=None,
-            description='Results:',
+            description='',
             disabled=False
         )
 
@@ -26,4 +30,16 @@ class Search(widgets.DOMWidget):
     @staticmethod
     def search(sender):
         print("Searched for", sender.value)
+        
+        queryString = "SELECT * WHERE { ?s ?p ?o. }"
+        sparql = SPARQLWrapper('http://server.nanopubs.lod.labs.vu.nl/')
+
+        sparql.setQuery(queryString)
+
+        try :
+           ret = sparql.query()
+        except :
+           print("Error when querying")
+
+        print('Result:', ret)
 
