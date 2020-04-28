699d94cfc17b81157196856ebc6495f710610897
Robin Richardson <raarichardson@gmail.com>
2020-04-28 17:08:41 +0200

    Move nanopub search to its own method

--- fairworkflows/fairworkflows/search.py
+++ fairworkflows/fairworkflows/search.py
@@ -23,31 +23,41 @@ class Search(widgets.DOMWidget):
         )
 
         def search(sender):
-           
-            # Current nanopubs server grlc api
-            apiurl = "http://grlc.nanopubs.lod.labs.vu.nl//api/local/local/find_nanopubs_with_text"
-
-            # Query the nanopub server for the specified text
-            searchparams = {'text': sender.value, 'graphpred': '', 'month': '', 'day': '', 'year': ''}
-            r = requests.get(apiurl, params=searchparams)
-
-            # Parse the resulting xml into a table
-            xmltree = et.ElementTree(et.fromstring(r.text))
-            xmlroot = xmltree.getroot()
-            namespace = '{http://www.w3.org/2005/sparql-results#}'
-            results = xmlroot.find(namespace + 'results')
-
-            nanopubs = []
-            for child in results:
-                nanopub = {}
-                for sub in child.iter(namespace + 'binding'):
-                    nanopub[sub.get('name')] = sub[0].text    
-                nanopubs.append(nanopub)
-                    
-            resultsbox.options = tuple(nanopubs)
-
+            results = Search.nanopubs(sender.value, max_num_results=3)                    
+            resultsbox.options = tuple(results)
+        
         searchtext.on_submit(search)
         display(searchtext, resultsbox)
 
+
+    @staticmethod
+    def nanopubs(searchtext, max_num_results=1000, apiurl='http://grlc.nanopubs.lod.labs.vu.nl//api/local/local/find_nanopubs_with_text'):
+        """
+        Searches the nanopub servers (at the specified grlc API) for any nanopubs matching the given search text,
+        up to max_num_results.
+        """
+
+        # Query the nanopub server for the specified text
+        searchparams = {'text': searchtext, 'graphpred': '', 'month': '', 'day': '', 'year': ''}
+        r = requests.get(apiurl, params=searchparams)
+
+        # Parse the resulting xml into a table
+        xmltree = et.ElementTree(et.fromstring(r.text))
+        xmlroot = xmltree.getroot()
+        namespace = '{http://www.w3.org/2005/sparql-results#}'
+        results = xmlroot.find(namespace + 'results')
+
+        nanopubs = []
+        for child in results:
+
+            nanopub = {}
+            for sub in child.iter(namespace + 'binding'):
+                nanopub[sub.get('name')] = sub[0].text
+            nanopubs.append(nanopub)
+
+            if len(nanopubs) >= max_num_results:
+                break
+
+        return nanopubs
 
 
