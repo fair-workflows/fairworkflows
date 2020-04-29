83b870453d83bb55bfc7c6cab40a8547de071ef0
Robin Richardson <raarichardson@gmail.com>
2020-04-29 16:58:31 +0200

    Add workflowhub CWL search functionality (with horrible hacky HTML parsing)

--- fairworkflows/fairworkflows/fairwidgets.py
+++ fairworkflows/fairworkflows/fairwidgets.py
@@ -4,7 +4,7 @@ from traitlets import Unicode, validate
 from IPython.display import display, HTML
 import tabulate
 
-from fairworkflows import Nanopub
+from fairworkflows import Nanopub, Workflowhub
 
 def search():
     """
@@ -16,9 +16,16 @@ def search():
     @interact(source=['nanopub', 'workflowhub', 'FAIR Data Point'], text='')
     def interactive_search(source='', text=''):
 
-        # Search for up to 3 nanopubs
-        results = Nanopub.search(text, max_num_results=3)
+        if source == 'nanopub':
+            # Search for up to 3 nanopubs
+            results = Nanopub.search(text, max_num_results=3)
+            table = [[r['v'], r['np'], r['date']] for r in results]
+
+        elif source == 'workflowhub':
+            # Search for up to 5 CWL workflows on the workflow hub
+            results = Workflowhub.search(text, max_num_results=5)
+            table = [[r['description'], r['url']] for r in results]
 
         # Output as table
-        table = [[r['v'], r['np'], r['date']] for r in results]
         display(HTML(tabulate.tabulate(table, tablefmt='html')))
+
