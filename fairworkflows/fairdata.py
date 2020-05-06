02c40ff8f88f3ff190f0d5043ed8286ca5509d76
Robin Richardson <raarichardson@gmail.com>
2020-05-06 14:10:09 +0200

    Give FairData class getters/setters for private vars

--- fairworkflows/fairdata.py
+++ fairworkflows/fairdata.py
@@ -5,9 +5,25 @@ class FairData:
     """
 
     def __init__(self, data=None, source_uri=None):
-        self.data = data
-        self.source_uri = source_uri
+        self._data = data
+        self._source_uri = source_uri
+
+    @property
+    def data(self):
+        return self._data
+
+    @data.setter
+    def data(self, data):
+        self._data = data
+
+    @property
+    def source_uri(self):
+        return self._source_uri
+
+    @source_uri.setter
+    def source_uri(self, source_uri):
+        self._source_uri = source_uri
 
     def __str__(self):
-        s = f'Source URI = {self.source_uri}\n{self.data}'
+        s = f'Source URI = {self._source_uri}\n{self._data}'
         return s
