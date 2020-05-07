4f7044f92e9aa99fcdbe3fb109a493ce05a8ef8a
Djura Smits <djura.smits@gmail.com>
2020-05-07 17:03:52 +0200

    Added docstrings Added example for executing RO crate in test.ipynb

--- fairworkflows/ro_crate.py
+++ fairworkflows/ro_crate.py
@@ -5,20 +5,34 @@ from typing import Union, Dict, List
 from . import cwl
 
 METADATA_FILE = 'ro-crate-metadata.jsonld'
-
 CONTEXT_KEY = '@context'
 
 
 class ROCrate:
+    """
+    Class that holds all necessary data on cwl RO crates.
+
+    """
 
     def __init__(self, path: Union[str, Path]):
+        """
+        Find the mandatory metadata file in path and infer the path to the main cwltool.
+
+        :param path:
+        """
         self.path = Path(path)
         self.metadata_path = self.path / METADATA_FILE
         self.metadata = self.metadata_path.read_text()
         self.metadata_graph = parse_metadata(self.metadata_path)
         self.cwltool = self.path/_get_cwltool_path(self.metadata_graph)
 
-    def run(self, inputs):
+    def run(self, inputs: Dict[str, any]):
+        """
+        Run the main cwltool of this RO crate.
+
+        :param inputs:
+        :return:
+        """
         cwl.run_workflow(wf_path=self.cwltool, inputs=inputs, base_dir=self.path)
 
     def __str__(self) -> str:
@@ -32,6 +46,8 @@ class ROCrate:
 def parse_metadata(path: Path) ->Dict[str, any]:
     """
     Parse RO-crate metadata as a regular json file.
+
+    json-ld libraries like the jsonld-rdflib are still quite immature and have a lot of issues.
 
     :param path:
     :return:
