9b917415df946685f941a2bc53c760bf5dcd4b90
Djura Smits <djura.smits@gmail.com>
2020-05-13 14:18:31 +0200

    Formatting

--- fairworkflows/cwl.py
+++ fairworkflows/cwl.py
@@ -1,17 +1,15 @@
 import argparse
 import logging
-from io import StringIO
+import tempfile
 from pathlib import Path
 from typing import List, Union, Dict, Optional
-import tempfile
 
 import cwlgen
-import cwltool.main as cwltool_main
 from scriptcwl import WorkflowGenerator
+from toil.cwl import cwltoil
 
 from config import CWL_WORKFLOW_DIR, CWL_STEPS_DIR, CWL_DIR
 from .exceptions import CWLException
-from toil.cwl import cwltoil
 
 _logger = logging.getLogger(__name__)
 
@@ -89,7 +87,6 @@ def run_workflow(wf_path: Union[Path, str], inputs: Dict[str, any], output_dir: 
     output_dir = str(output_dir)
 
     print('AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA', output_dir, log_path)
-
 
     wf_path = str(wf_path)
 
