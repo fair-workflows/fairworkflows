63bf3a0f48dc87f9e11575c9b2c3e1d014ca89fd
Robin Richardson <raarichardson@gmail.com>
2020-05-14 14:47:30 +0200

    Remove now obsolete ipywidgets (now this functionality is found in the jupyter lab frontend extension

--- fairworkflows/__init__.py
+++ fairworkflows/__init__.py
@@ -4,7 +4,4 @@ from .fairdata import FairData
 from .nanopub import Nanopub
 from .ro_crate import ROCrate
 from .workflowhub import Workflowhub
-from .fairwidgets import search
-from .step import Step
-from .load import Load
 
