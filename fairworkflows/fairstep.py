7b9b69cee348e670a1bdcabb8bddbb74cafa0a5b
Robin Richardson <raarichardson@gmail.com>
2020-08-26 17:03:32 +0200

    Check for necessary triples in rdflib directly for now

--- fairworkflows/fairstep.py
+++ fairworkflows/fairstep.py
@@ -1,7 +1,11 @@
+from .nanopub import Nanopub
 import pyshacl
 import rdflib
+from rdflib import RDF, DCTERMS
 
 class FairStep:
+
+    DEFAULT_STEP_URI = 'http://purl.org/nanopub/temp/mynanopub#step'
 
     def __init__(self, step_rdf:rdflib.Graph = None, uri=None):
         
@@ -10,15 +14,41 @@ class FairStep:
         else:
             self.rdf = rdflib.Graph()
 
+        if uri:
+            self.uri = uri
+        else:
+            self.uri = self.DEFAULT_STEP_URI
+
+        self.this_step = rdflib.URIRef(self.uri)
+        
+
     def validate(self):
         """
         Checks whether this step rdf has sufficient information required of
         a step in the Plex ontology.
         """
-        shacl_graph = rdflib.Graph()
 
-        r = pyshacl.validate(self.rdf, shacl_graph=shacl_graph)
-        print(r)
+        # Use shacl later (once the schema is properly defined)
+        #
+        #shacl_graph = rdflib.Graph()
+        #ex = rdflib.Namespace('http://www.example.com/ns#')
+        #shacl_graph.add( (ex.PlexStepShape, RDF.type, Nanopub.PPLAN.Plan) )
+        #conforms, results_graph, results_txt = pyshacl.validate(self.rdf, shacl_graph=shacl_graph)
+        #print(results_txt)
+        #return conforms
+
+        conforms = True
+
+        necessary_triples = [
+            (self.this_step, RDF.type, Nanopub.PPLAN.Step), # Must be a pplan:Step
+            (self.this_step, DCTERMS.description, None)     # Must have a dcterms:description
+        ]
+        for t in necessary_triples:
+            if t not in self.rdf:
+                print("Plex step is missing triple of the form:", t)
+                conforms = False
+
+        return conforms
 
 
 #    def is_manual(self):
