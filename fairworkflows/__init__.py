c739ba68f83044811bd0b8ce7f57d1c1f01c21d9
Robin Richardson <raarichardson@gmail.com>
2020-08-26 15:58:45 +0200

    Add basic fairstep class, with (empty) shacl validation. Get rid of cwltool etc due to ancient rdflib requirements.

--- fairworkflows/__init__.py
+++ fairworkflows/__init__.py
@@ -1,4 +1,6 @@
 from ._version import __version__
-from .fairstep import fairstep
+from .fairstep import FairStep
 from .nanopub import Nanopub
 from .manualstep import manualstep
+
+from .cwlstep import cwlstep
---
+++ fairworkflows/fairworkflow.py
@@ -0,0 +1,10 @@
+import rdflib
+
+class FairWorkflow:
+
+    def __init__():
+        steps = []
+
+    def add_step():
+        
+        
