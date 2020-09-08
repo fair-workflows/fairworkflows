8e8a864b50a82083fee9a404040b87aedf8771c6
Sven van der Burg <s.vanderburg@esciencecenter.nl>
2020-09-08 13:27:28 +0200

    Add comment to test

--- tests/test_rdf_wrapper.py
+++ tests/test_rdf_wrapper.py
@@ -11,10 +11,13 @@ class TestRdfWrapper:
         test_predicate = rdflib.RDF.type
         test_value_1 = rdflib.term.Literal('test_value_1')
         test_value_2 = rdflib.term.Literal('test_value_2')
+
         wrapper = RdfWrapper(uri='test')
         wrapper.set_attribute(test_predicate, test_value_1)
         assert wrapper.get_attribute(test_predicate) == test_value_1
 
+        # Setting the attribute another time should overwrite, and throw a
+        # warning.
         with warnings.catch_warnings(record=True) as w:
             warnings.simplefilter("always")
             wrapper.set_attribute(test_predicate, test_value_2)
