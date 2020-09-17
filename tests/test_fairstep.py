369a1e338ea4e2f584da595bb515e841e0f10269
Sven van der Burg <s.vanderburg@esciencecenter.nl>
2020-09-17 09:13:41 +0200

    refactor tests in test_fairstep.py

--- tests/test_fairstep.py
+++ tests/test_fairstep.py
@@ -15,26 +15,30 @@ def nanopub_server_unavailable():
 
 
 class TestFairStep:
-    def test_construction(self):
+    def test_inputs_outputs(self):
+        test_inputs = ['test.org#input1', 'test.org#input2']
+        test_outputs = ['test.org#output1', 'test.org#output2']
         step = FairStep()
-        step.inputs = ['test.org#input1', 'test.org#input2']
-        step.outputs = ['test.org#output1', 'test.org#output2']
+        step.inputs = test_inputs
+        step.outputs = test_outputs
         assert len(step.inputs) == 2
         assert len(step.outputs) == 2
-    
-    def test_overwrite_inputs(self):
-        step = FairStep()
-        step.inputs = ['test.org#input1', 'test.org#input2']
-        assert len(step.inputs) == 2
-        step.inputs = ['test.org#input3']
-        assert len(step.inputs) == 1
+        for input in step.inputs:
+            assert str(input) in test_inputs
+        for output in step.outputs:
+            assert str(output) in test_outputs
 
-    def test_overwrite_outputs(self):
-        step = FairStep()
-        step.outputs = ['test.org#output1', 'test.org#output2']
-        assert len(step.outputs) == 2
-        step.outputs = ['test.org#output3']
+        # test overwriting
+        new_input = 'test.org#input3'
+        new_output = 'test.org#output3'
+        step.inputs = [new_input]
+        step.outputs = [new_output]
+        assert len(step.inputs) == 1
         assert len(step.outputs) == 1
+        for input in step.inputs:
+            assert str(input) == new_input
+        for output in step.outputs:
+            assert str(output) == new_output
 
     @pytest.mark.flaky(max_runs=10)
     @pytest.mark.skipif(nanopub_server_unavailable(), reason=SERVER_UNAVAILABLE)
@@ -54,8 +58,6 @@ class TestFairStep:
             step = FairStep.from_nanopub(uri=uri)
             assert step is not None
             step.validate()
-            assert step.is_pplan_step
-            assert step.description is not None
             assert step.is_manual_task
             assert not step.is_script_task
 
@@ -79,8 +81,6 @@ class TestFairStep:
             step = FairStep.from_nanopub(uri=uri)
             assert step is not None
             step.validate()
-            assert step.is_pplan_step
-            assert step.description is not None
             assert step.is_manual_task
             assert not step.is_script_task
 
@@ -95,8 +95,6 @@ class TestFairStep:
 
         assert step is not None
         step.validate()
-        assert step.is_pplan_step
-        assert step.description is not None
         assert not step.is_manual_task
         assert step.is_script_task
 
