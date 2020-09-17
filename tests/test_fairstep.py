d4b7aa665f3b95e6a121cb7cd5d1de0809117d00
Sven van der Burg <s.vanderburg@esciencecenter.nl>
2020-09-17 08:56:19 +0200

    Organize FairStep tests in a class

--- tests/test_fairstep.py
+++ tests/test_fairstep.py
@@ -14,102 +14,97 @@ def nanopub_server_unavailable():
     return response.status_code == BAD_GATEWAY
 
 
-def test_fairstep_construction():
-    step = FairStep()
-    step.inputs = ['test.org#input1', 'test.org#input2']
-    step.outputs = ['test.org#output1', 'test.org#output2']
-    assert len(step.inputs) == 2
-    assert len(step.outputs) == 2
+class TestFairStep:
+    def test_construction(self):
+        step = FairStep()
+        step.inputs = ['test.org#input1', 'test.org#input2']
+        step.outputs = ['test.org#output1', 'test.org#output2']
+        assert len(step.inputs) == 2
+        assert len(step.outputs) == 2
+    
+    def test_overwrite_inputs(self):
+        step = FairStep()
+        step.inputs = ['test.org#input1', 'test.org#input2']
+        assert len(step.inputs) == 2
+        step.inputs = ['test.org#input3']
+        assert len(step.inputs) == 1
 
+    def test_overwrite_outputs(self):
+        step = FairStep()
+        step.outputs = ['test.org#output1', 'test.org#output2']
+        assert len(step.outputs) == 2
+        step.outputs = ['test.org#output3']
+        assert len(step.outputs) == 1
 
-def test_fairstep_overwrite_inputs():
-    step = FairStep()
-    step.inputs = ['test.org#input1', 'test.org#input2']
-    assert len(step.inputs) == 2
-    step.inputs = ['test.org#input3']
-    assert len(step.inputs) == 1
+    @pytest.mark.flaky(max_runs=10)
+    @pytest.mark.skipif(nanopub_server_unavailable(), reason=SERVER_UNAVAILABLE)
+    def test_construction_from_nanopub(self):
+        """
+            Check that we can load a FairStep from known nanopub URIs for manual steps,
+            that validation works, etc
+        """
 
+        nanopub_uris = [
+            'http://purl.org/np/RACLlhNijmCk4AX_2PuoBPHKfY1T6jieGaUPVFv-fWCAg#step',
+            'http://purl.org/np/RANBLu3UN2ngnjY5Hzrn7S5GpqFdz8_BBy92bDlt991X4#step',
+            'http://purl.org/np/RA5D8NzM2OXPZAWNlADQ8hZdVu1k0HnmVmgl20apjhU8M#step'
+        ]
 
-def test_fairstep_overwrite_outputs():
-    step = FairStep()
-    step.outputs = ['test.org#output1', 'test.org#output2']
-    assert len(step.outputs) == 2
-    step.outputs = ['test.org#output3']
-    assert len(step.outputs) == 1
+        for uri in nanopub_uris:
+            step = FairStep.from_nanopub(uri=uri)
+            assert step is not None
+            step.validate()
+            assert step.is_pplan_step
+            assert step.description is not None
+            assert step.is_manual_task
+            assert not step.is_script_task
 
+    @pytest.mark.flaky(max_runs=10)
+    @pytest.mark.skipif(nanopub_server_unavailable(), reason=SERVER_UNAVAILABLE)
+    def test_construction_from_nanopub_without_fragment(self):
+        """
+            Check that we can load a FairStep from known nanopub URIs also
+            in cases where the full step URI is not used (i.e. missing a fragment).
+            In other words, only the nanopub URI is given, but not the URI of the
+            step itself within that nanopub.
+        """
 
-@pytest.mark.flaky(max_runs=10)
-@pytest.mark.skipif(nanopub_server_unavailable(), reason=SERVER_UNAVAILABLE)
-def test_fairstep_from_nanopub():
-    """
-        Check that we can load a FairStep from known nanopub URIs for manual steps,
-        that validation works, etc
-    """
+        nanopub_uris = [
+            'http://purl.org/np/RACLlhNijmCk4AX_2PuoBPHKfY1T6jieGaUPVFv-fWCAg',
+            'http://purl.org/np/RANBLu3UN2ngnjY5Hzrn7S5GpqFdz8_BBy92bDlt991X4',
+            'http://purl.org/np/RA5D8NzM2OXPZAWNlADQ8hZdVu1k0HnmVmgl20apjhU8M'
+        ]
 
-    nanopub_uris = [
-        'http://purl.org/np/RACLlhNijmCk4AX_2PuoBPHKfY1T6jieGaUPVFv-fWCAg#step',
-        'http://purl.org/np/RANBLu3UN2ngnjY5Hzrn7S5GpqFdz8_BBy92bDlt991X4#step',
-        'http://purl.org/np/RA5D8NzM2OXPZAWNlADQ8hZdVu1k0HnmVmgl20apjhU8M#step'
-    ]
+        for uri in nanopub_uris:
+            step = FairStep.from_nanopub(uri=uri)
+            assert step is not None
+            step.validate()
+            assert step.is_pplan_step
+            assert step.description is not None
+            assert step.is_manual_task
+            assert not step.is_script_task
 
-    for uri in nanopub_uris:
-        step = FairStep.from_nanopub(uri=uri)
+    def test_construction_from_function(self):
+        def add(a: int, b: int):
+            """
+            Computational step adding two ints together.
+            """
+            return a + b
+
+        step = FairStep.from_function(func=add)
+
         assert step is not None
         step.validate()
         assert step.is_pplan_step
         assert step.description is not None
-        assert step.is_manual_task
-        assert not step.is_script_task
+        assert not step.is_manual_task
+        assert step.is_script_task
 
+        assert step.__str__() is not None
+        assert len(step.__str__()) > 0
+        assert step.rdf is not None
 
-@pytest.mark.flaky(max_runs=10)
-@pytest.mark.skipif(nanopub_server_unavailable(), reason=SERVER_UNAVAILABLE)
-def test_fairstep_from_nanopub_without_fragment():
-    """
-        Check that we can load a FairStep from known nanopub URIs also
-        in cases where the full step URI is not used (i.e. missing a fragment).
-        In other words, only the nanopub URI is given, but not the URI of the
-        step itself within that nanopub.
-    """
-
-    nanopub_uris = [
-        'http://purl.org/np/RACLlhNijmCk4AX_2PuoBPHKfY1T6jieGaUPVFv-fWCAg',
-        'http://purl.org/np/RANBLu3UN2ngnjY5Hzrn7S5GpqFdz8_BBy92bDlt991X4',
-        'http://purl.org/np/RA5D8NzM2OXPZAWNlADQ8hZdVu1k0HnmVmgl20apjhU8M'
-    ]
-
-    for uri in nanopub_uris:
-        step = FairStep.from_nanopub(uri=uri)
-        assert step is not None
-        step.validate()
-        assert step.is_pplan_step
-        assert step.description is not None
-        assert step.is_manual_task
-        assert not step.is_script_task
-
-
-def test_fairstep_from_function():
-    def add(a: int, b: int):
-        """
-        Computational step adding two ints together.
-        """
-        return a + b
-
-    step = FairStep.from_function(func=add)
-
-    assert step is not None
-    step.validate()
-    assert step.is_pplan_step
-    assert step.description is not None
-    assert not step.is_manual_task
-    assert step.is_script_task
-
-    assert step.__str__() is not None
-    assert len(step.__str__()) > 0
-    assert step.rdf is not None
-
-
-def test_validation():
-    step = FairStep(uri='http://www.example.org/step')
-    with pytest.raises(AssertionError):
-        step.validate()
+    def test_validation(self):
+        step = FairStep(uri='http://www.example.org/step')
+        with pytest.raises(AssertionError):
+            step.validate()
