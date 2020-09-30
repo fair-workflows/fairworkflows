701962e17b21f013947f923ca107c860689ba3b2
Robin Richardson <raarichardson@gmail.com>
2020-09-30 16:30:21 +0200

    Update setup.py with long description of library, so that pypI picks it up.

--- setup.py
+++ setup.py
@@ -21,28 +21,29 @@ def get_version(rel_path):
     else:
         raise RuntimeError("Unable to find version string.")
 
-
 setup(name='fairworkflows',
-      version=get_version('fairworkflows/_version.py'),
-      description='FAIRWorkflows python library',
-      author='Robin Richardson, Djura Smits, Sven van den Burg',
-      author_email='r.richardson@esciencecenter.nl',
-      url='https://github.com/fair-workflows/fairworkflows/',
-      install_requires=open("requirements.txt", "r").readlines(),
-      packages=['fairworkflows'],
-      package_data={
-          'fairworkflows': [
-              'np'
-          ],
-      },
-      include_package_data=True,
-      extras_require={
-          'dev': open('requirements_dev.txt', 'r').readlines()
-      },
-      classifiers=[
-          "Programming Language :: Python :: 3",
-          "License :: OSI Approved :: Apache Software License",
-          "Operating System :: OS Independent"
-      ],
-      python_requires='>=3.6'
-      )
+    version=get_version('fairworkflows/_version.py'),
+    description='FAIRWorkflows python library',
+    long_description=open("README.md", "r").readlines(),
+    long_description_content_type='text/markdown',
+    author='Robin Richardson, Djura Smits, Sven van den Burg',
+    author_email='r.richardson@esciencecenter.nl',
+    url='https://github.com/fair-workflows/fairworkflows/',
+    install_requires=open("requirements.txt", "r").readlines(),
+    packages=['fairworkflows'],
+    package_data={
+        'fairworkflows': [
+            'np'
+        ],
+    },
+    include_package_data=True,
+    extras_require={
+        'dev': open('requirements_dev.txt', 'r').readlines()
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: Apache Software License",
+        "Operating System :: OS Independent"
+    ],
+    python_requires='>=3.6'
+)
