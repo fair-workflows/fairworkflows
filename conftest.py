cb3882221e05e4b9d1ef4270a2871f88bb726cc7
Sven van der Burg <s.vanderburg@esciencecenter.nl>
2020-10-23 15:32:21 +0200

    Simplify code of skip_if_nanopub_server_unavailable

--- conftest.py
+++ conftest.py
@@ -3,13 +3,7 @@ import requests
 
 NANOPUB_SERVER = 'http://purl.org/np/'
 
-
-def nanopub_server_available():
-    response = requests.get(NANOPUB_SERVER)
-    return response.status_code == 200
-
-
 skip_if_nanopub_server_unavailable = (
-    pytest.mark.skipif(not nanopub_server_available(),
+    pytest.mark.skipif(requests.get(NANOPUB_SERVER).status_code != 200,
                        reason='Nanopub server is unavailable'))
 
