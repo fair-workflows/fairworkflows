329d2b0a0d80a5669ed23ade32e116e008c1b692
Sven van der Burg <s.vanderburg@esciencecenter.nl>
2020-12-29 08:46:30 +0100

    Remove add_step decorator

--- fairworkflows/fairworkflow.py
+++ fairworkflows/fairworkflow.py
@@ -385,19 +385,3 @@ class FairWorkflow(RdfWrapper):
         s += self._rdf.serialize(format='trig').decode('utf-8')
         return s
 
-
-def add_step(fw: FairWorkflow):
-    """
-    Decorator that, upon execution, will convert a function to a FairStep, and add it to the
-    given FairWorkflow, 'fw'
-    """
-
-    def decorated_step(function):
-
-        def wrapped_step(*args, **kwargs):
-            fw.add(FairStep.from_function(function=function))
-            function(*args, **kwargs)
-
-        return wrapped_step
-
-    return decorated_step
