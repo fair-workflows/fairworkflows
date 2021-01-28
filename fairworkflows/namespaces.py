f50909cdbdaa1158646bca472935fb258cf2071c
Robin Richardson <raarichardson@gmail.com>
2021-01-28 11:59:41 +0100

    Make execute() return a Publication containing the retrospective provenance

--- fairworkflows/namespaces.py
+++ fairworkflows/namespaces.py
@@ -5,3 +5,9 @@ PPLAN = rdflib.Namespace("http://purl.org/net/p-plan#")
 DUL = rdflib.Namespace("http://www.ontologydesignpatterns.org/ont/dul/DUL.owl#")
 BPMN = rdflib.Namespace("http://dkm.fbk.eu/index.php/BPMN2_Ontology#")
 PWO = rdflib.Namespace("http://purl.org/spar/pwo/")
+
+"""
+Namespace for
+`http://www.w3.org/ns/prov#`
+"""
+PROV = rdflib.Namespace("http://www.w3.org/ns/prov#")
